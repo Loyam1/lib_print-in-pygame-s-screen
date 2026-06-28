b54fe3387f936b334b2fc24202cbdb0a046c3e93
mayol_qo0tzvv <mayol.picard@gmx.fr>
2026-06-28 21:39:13 +0200

    V1

---
+++ setup.py
@@ -0,0 +1,18 @@
+setup(
+    name='print_in_screen',  # Replace with your package’s name
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        pygame
+    ],
+    author='Loyam',
+    author_email='mayol.picard@gmx.fr',
+    description='A library for displaying text more easily in PyGame',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # License type
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+
+)
