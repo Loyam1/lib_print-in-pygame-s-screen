1cc0d7d6499bb7799f6fd0249fe96a7518c7208e
mayol_qo0tzvv <mayol.picard@gmx.fr>
2026-06-28 21:38:51 +0200

    structur

--- multiples/print_in_screen.py
+++ multiples/print_in_screen.py
@@ -1,2 +1,14 @@
 import pygame
-def print_in_screen (instruction):#instruction is a dictionnary
+def print_in_screen (user_instruction, screen):#screen is the pygame.display.set_mode and instruction is a dictionnary {"text": "str", "coordinates": [x, y], "size": int, "color": rgb or "name"} "text" and "coordinates" are required, but the others are not
+  intruction = {"text": user_instruction["text"], "coordinates": user_instruction["coordinates"]}
+  if user_instruction.get("size",30) != 30:
+    intruction["size"] = user_instruction["size"]
+  else:
+    intruction["size"] = 30
+  if user_instruction.get("color","white") != "white":
+    intruction["color"] = user_instruction["color"]
+  else:
+    intruction["color"] = "white"
+  pygame.init()
+  font = pygame.font.Font(None,intruction["size"])
+  screen.blit(font.render(intruction["text"], True, intruction["color"]),intruction["coordinates"])
